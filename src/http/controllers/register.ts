2a298767980a308503924117a5b3fb21a4c6c284
becardine <bealvescardine@gmail.com>
2023-06-16 01:04:09 -0300

    feat: standardize errors in use cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -19,7 +20,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (err) {
-    return reply.code(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.code(409).send({
+        message: err.message,
+      })
+    }
+
+    return reply.status(500).send() // todo: fix me
   }
 
   return reply.status(201).send()
