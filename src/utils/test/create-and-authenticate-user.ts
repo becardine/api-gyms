f324fa4bb5ef4fbab75c2c11f87a968731f84228
becardine <bealvescardine@gmail.com>
2023-06-20 03:06:18 -0300

    feat: create role for users and middleware

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -12,9 +12,9 @@ export async function createAndAuthenticateUser(
       name: 'John Doe',
       email: 'johndoe@example.com',
       password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
     },
   })
-  // role: isAdmin ? 'ADMIN' : 'MEMBER',
 
   const authResponse = await request(app.server).post('/sessions').send({
     email: 'johndoe@example.com',
